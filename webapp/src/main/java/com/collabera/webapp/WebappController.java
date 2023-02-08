45f341c2be4aa97773cfa1ef4e75eea959917205
AdlinaKadir <124658049+AdlinaKadir@users.noreply.github.com>
2023-02-09 01:14:56 +0800

    Add files via upload
    WEB APPLICATION

--- webapp/src/main/java/com/collabera/webapp/WebappController.java
+++ webapp/src/main/java/com/collabera/webapp/WebappController.java
@@ -19,57 +19,57 @@ public class WebappController {
     private RestTemplate restTemplate;
     private WebappController imageService;
 
-    @GetMapping("/")
+    @GetMapping("/") //to index page
     public String index(Model model)
     {
         User user = new User();
-        model.addAttribute("user", user);
+        model.addAttribute("user", user); //to send attribute user
         return "index";
     }
 
     /*USER*/
 
-    @GetMapping("/useradmin")
+    @GetMapping("/useradmin") //to user page for admin
     public String useradmin()
     {
         return "useradmin";
     }
-    @GetMapping("/register")
+    @GetMapping("/register") //to register page for admin
     public String registerpage()
     {
         return "register";
     }
 
-    @GetMapping("/home")
+    @GetMapping("/home") //to home page for user
     public String home(Model model)
     {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<Movie> entity = new HttpEntity<Movie>(headers);
-        List<Movie> dataList = restTemplate.exchange("http://localhost:8083/movies", HttpMethod.GET, entity, List.class).getBody();
-        model.addAttribute("dataList", dataList);
+        HttpHeaders headers = new HttpHeaders(); // create object
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON)); // set object as array
+        HttpEntity<Movie> entity = new HttpEntity<Movie>(headers); // send object to entity
+        List<Movie> dataList = restTemplate.exchange("http://localhost:8083/movies", HttpMethod.GET, entity, List.class).getBody(); //entity to movie API
+        model.addAttribute("dataList", dataList); // set dataList to model attribute
         return "home";
     }
 
-    @GetMapping("/homeadmin")
+    @GetMapping("/homeadmin") //to home for admin
     public String homeadmin()
     {
         return "homeadmin";
     }
 
-    @GetMapping("/users")
+    @GetMapping("/users") // to view all user page
     public String findallUser(Model model)
     {
         //fetch data of all user from user table via user-service
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<User> entity = new HttpEntity<User>(headers);
-        List<User> dataList = restTemplate.exchange("http://localhost:8085/users", HttpMethod.GET, entity, List.class).getBody();
+        HttpHeaders headers = new HttpHeaders(); // set object
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON)); //set object to array
+        HttpEntity<User> entity = new HttpEntity<User>(headers); //set object to entity
+        List<User> dataList = restTemplate.exchange("http://localhost:8085/users", HttpMethod.GET, entity, List.class).getBody(); //entity to user API
         model.addAttribute("dataList", dataList); // data is insert here to be use by thymeleaf in html
         return "viewalluser";
     }
 
-    @PostMapping("/register")
+    @PostMapping("/register") // after click register
     public String register(@ModelAttribute("user") User user, Model model)
     {
         String name = user.getName(); // get username input
@@ -83,10 +83,10 @@ public class WebappController {
         var encodedPassword = encoder.encode(myPassword); //encrypt the password
 
         /*Find data with String name*/
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<User> entitycheck = new HttpEntity<User>(headers);
-        User find = restTemplate.exchange("http://localhost:8085/findbyname/"+name, HttpMethod.GET, entitycheck, User.class).getBody();
-        Optional<User> userdata = Optional.ofNullable(find);
+        HttpHeaders headers = new HttpHeaders();// set object
+        HttpEntity<User> entitycheck = new HttpEntity<User>(headers);// set object to entity
+        User find = restTemplate.exchange("http://localhost:8085/findbyname/"+name, HttpMethod.GET, entitycheck, User.class).getBody();//entity to user API to find data by name
+        Optional<User> userdata = Optional.ofNullable(find);//optional object to hold result method find
 
         if (userdata.isPresent()) // if the data for String name exist
         {
@@ -97,7 +97,7 @@ public class WebappController {
         else
         {
             user = new User(id, name, encodedPassword, email, position); // get all the input data
-            HttpEntity<User> entity = new HttpEntity<User>(user);
+            HttpEntity<User> entity = new HttpEntity<User>(user); //set data to entity
             restTemplate.exchange("http://localhost:8085/register", HttpMethod.POST, entity, User.class).getBody(); // add data to user table
             return "useradmin"; // redirect to useradmin page
         }
@@ -108,10 +108,10 @@ public class WebappController {
     {
         // Find user by username
         String name = user.getName(); //get the username enter by user
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<User> entity = new HttpEntity<User>(headers);
+        HttpHeaders headers = new HttpHeaders();// create object
+        HttpEntity<User> entity = new HttpEntity<User>(headers); //set object to entity
         User find = restTemplate.exchange("http://localhost:8085/findbyname/"+name, HttpMethod.GET, entity, User.class).getBody(); // find the username in database
-        Optional<User> userdata = Optional.ofNullable(find);
+        Optional<User> userdata = Optional.ofNullable(find); //optional object to hold result method find
 
         //encrypted password
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(14);
@@ -157,28 +157,28 @@ public class WebappController {
 
     /*MOVIE*/
 
-    @GetMapping("/movieadmin")
+    @GetMapping("/movieadmin") // to movie page for admin
     public String movieadmin()
     {
         return "movieadmin";
     }
 
-    @GetMapping("/addmovie")
+    @GetMapping("/addmovie") // to add page for admin
     public String addmoviepage()
     {
         return "addmovie";
     }
 
-    @PostMapping("/addmovie")
+    @PostMapping("/addmovie") //after admin click add movie
     public String admovie(@ModelAttribute("movie") Movie movie)
     {
         //add the data to movie table via movie-service
         HttpEntity<Movie> entity = new HttpEntity<Movie>(movie);
-        restTemplate.exchange("http://localhost:8083/addMovie", HttpMethod.POST, entity, Movie.class).getBody();
+        restTemplate.exchange("http://localhost:8083/addMovie", HttpMethod.POST, entity, Movie.class).getBody(); //add Movie API
         return "movieadmin";
     }
 
-    @GetMapping("/movieinfo")
+    @GetMapping("/movieinfo") //to movie info page are user select a movie
     public String movieinfopage(@RequestParam("id") int id, Model model)
     {
 
@@ -208,16 +208,6 @@ public class WebappController {
         return "movieinfo";
     }
 
-    // Java Controller
-    @GetMapping("/records/{id}")
-    public String showRecord(int id, Model model) {
-        HttpHeaders headers2 = new HttpHeaders();
-        HttpEntity<Movie> entity2 = new HttpEntity<Movie>(headers2);
-        Movie list = restTemplate.exchange("http://localhost:8083/findbytmdbid/{id}"+id, HttpMethod.GET, entity2, Movie.class).getBody();
-        model.addAttribute("recordId", list.getTmdbId());
-        return "home";
-    }
-
     /*Rating*/
     @PostMapping("/rate")
     public String addRate(@ModelAttribute("rating") Rating rating, Model model)
@@ -231,6 +221,7 @@ public class WebappController {
         HttpEntity<Rating> entity = new HttpEntity<Rating>(rating); //assign rating to entity
         restTemplate.exchange("http://localhost:8084/addrating", HttpMethod.POST, entity, Rating.class).getBody(); //entity data to be added to db by rating-service
 
+        /*List all movie to display at home*/
         HttpHeaders headers3 = new HttpHeaders();
         headers3.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<Movie> entity3 = new HttpEntity<Movie>(headers3);
@@ -243,8 +234,6 @@ public class WebappController {
     @GetMapping("/fav")
     public String addFav(@RequestParam("id") int id,@RequestParam("userId") int userId, @ModelAttribute("favourite") Favourite favourite, Model model)
     {
-
-
         // get id for favourie (it will auto increment)
         int idf = favourite.getId();
 
@@ -252,7 +241,81 @@ public class WebappController {
         HttpEntity<Favourite> entity = new HttpEntity<Favourite>(favourite); //assign the data to the entity
         restTemplate.exchange("http://localhost:8087/addFav", HttpMethod.POST, entity, Favourite.class).getBody(); //entity data send to fav-service to add in db
 
+        /*find favourite data for the current user*/
+        HttpHeaders headers = new HttpHeaders(); //create object
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON)); //object as array
+        HttpEntity<Favourite> entity2 = new HttpEntity<Favourite>(headers); // object to entity
+        List<Favourite> favList = restTemplate.exchange("http://localhost:8087/favs/"+userId, HttpMethod.GET, entity2, new ParameterizedTypeReference<List<Favourite>>(){}).getBody();
+
+        /*find movie detail based on favourite data result*/
+        HttpHeaders headers3 = new HttpHeaders();
+        headers3.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Movie> entity3 = new HttpEntity<Movie>(headers3);
+        List<Movie> dataListmovie = new ArrayList<>();
+        for (Favourite favourite1 : favList) {
+            int movieId = favourite1.getMovieId();
+            List<Movie> favMovies = restTemplate.exchange("http://localhost:8083/movies/" + movieId, HttpMethod.GET, entity3, new ParameterizedTypeReference<List<Movie>>() {}).getBody();
+            dataListmovie.addAll(favMovies);
+        }
+        model.addAttribute("dataList", dataListmovie);
+
         return "favourite";// redirect to favourite page
+    }
+
+    @GetMapping("/viewfavs") // page to view all favourites for the current user
+    public String findallFav(Model model, HttpSession session)
+    {
+        /*find favourite data for the current user*/
+        Integer userId = (Integer) session.getAttribute("userid");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Favourite> entity2 = new HttpEntity<Favourite>(headers);
+        List<Favourite> favList = restTemplate.exchange("http://localhost:8087/favs/"+userId, HttpMethod.GET, entity2, new ParameterizedTypeReference<List<Favourite>>(){}).getBody();
+        List<Integer> dataListid = new ArrayList<>();
+        for (Favourite favourite : favList){
+            int favid = favourite.getId();
+            dataListid.add(favid);
+        }
+        model.addAttribute("dataid", dataListid);
+
+        /*find movie detail based on favourite data result*/
+        HttpHeaders headers3 = new HttpHeaders();
+        headers3.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Movie> entity3 = new HttpEntity<Movie>(headers3);
+        List<Movie> dataListmovie = new ArrayList<>();
+        for (Favourite favourite1 : favList) {
+            int movieId = favourite1.getMovieId();
+            List<Movie> favMovies = restTemplate.exchange("http://localhost:8083/movies/" + movieId, HttpMethod.GET, entity3, new ParameterizedTypeReference<List<Movie>>() {}).getBody();
+            dataListmovie.addAll(favMovies);
+        }
+        model.addAttribute("dataList", dataListmovie);
+        return "favourite";
+    }
+
+    @PostMapping("/del")
+    public String delfav(@RequestParam int id, HttpSession session,Model model)
+    {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Integer> entity = new HttpEntity<Integer>(headers);
+        String product = restTemplate.exchange("http://localhost:8087/delete/"+id, HttpMethod.DELETE, entity, String.class).getBody();
+
+        Integer userId = (Integer) session.getAttribute("userid");
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Favourite> entity2 = new HttpEntity<Favourite>(headers2);
+        List<Favourite> favList = restTemplate.exchange("http://localhost:8087/favs/"+userId, HttpMethod.GET, entity2, new ParameterizedTypeReference<List<Favourite>>(){}).getBody();
+
+        HttpHeaders headers3 = new HttpHeaders();
+        headers3.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Movie> entity3 = new HttpEntity<Movie>(headers3);
+        List<Movie> dataListmovie = new ArrayList<>();
+        for (Favourite favourite1 : favList) {
+            int movieId = favourite1.getMovieId();
+            List<Movie> favMovies = restTemplate.exchange("http://localhost:8083/movies/" + movieId, HttpMethod.GET, entity3, new ParameterizedTypeReference<List<Movie>>() {}).getBody();
+            dataListmovie.addAll(favMovies);
+        }
+        model.addAttribute("dataList", dataListmovie);
+        return "favourite";
     }
 
 }
